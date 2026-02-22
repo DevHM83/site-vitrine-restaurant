868b4c1c09a8e93852b83d2a507dcc14f255d526
Hakim Dublin <hakim.moussouni@oclock.school>
2026-02-22 13:11:54 +0100

    feat: add dishes data

---
+++ public/js/dishes.js
@@ -0,0 +1,56 @@
+export const dishes = [
+  {
+    name: "Lasagnes à la bolognaise",
+    country: "Italie",
+    ingredients: "Pâtes, Viande Hachée, Sauce Béchamel",
+    description:
+      "Pâtes feuilletées, sauce bolognaise (viande hachée, tomates), béchamel et parmesan. Servi avec une salade verte.",
+    price: 14.9,
+    image: "public/images/lasagnes.jpg",
+  },
+  {
+    name: "Couscous royal",
+    country: "Algérie",
+    ingredients: "Semoule, Légumes, Merguez",
+    description:
+      "Semoule fine, légumes (carottes, courgettes, navets), merguez, poulet et boulette de viande. Accompagné de sauce piquante.",
+    price: 16.5,
+    image: "public/images/couscous.jpg",
+  },
+  {
+    name: "Thiéboudienne",
+    country: "Sénégal",
+    ingredients: "Riz, Poisson, Sauce Tomate",
+    description:
+      "Riz cuit avec du poisson frais, des légumes (aubergine, chou, carotte) et une sauce tomate épicée. Un classique sénégalais !",
+    price: 15.9,
+    image: "public/images/thieboudienne.jpg",
+  },
+  {
+    name: "Tacos al pastor",
+    country: "Mexique",
+    ingredients: "Tortillas, Viande Marinée, Ananas",
+    description:
+      "Tortillas de maïs, viande de porc marinée, ananas grillé, oignon rouge et coriandre. Servi avec une sauce verte ou rouge.",
+    price: 12.9,
+    image: "public/images/tacos.jpg",
+  },
+  {
+    name: "Butter chicken",
+    country: "Inde",
+    ingredients: "Poulet, Sauce Tomate, Epices",
+    description:
+      "Poulet mariné dans du yaourt et des épices, cuit au four puis mijoté dans une sauce tomate crémeuse. Servi avec du riz basmati.",
+    price: 17.5,
+    image: "public/images/butter_chicken.jpg",
+  },
+  {
+    name: "Ramen tonkotsu",
+    country: "Japon",
+    ingredients: "Nouilles, Bouillon de Porc, Oeuf Mariné",
+    description:
+      "Nouilles fraîches dans un bouillon de porc riche, accompagné de tranches de porc, œuf mariné, pousses de bambou et algues nori.",
+    price: 18.9,
+    image: "public/images/ramen.jpg",
+  },
+];
